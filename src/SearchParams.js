03385d7c38d1fd51ef466f26cd2e95252e771712
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-06-28 15:11:15 -0500

    Hooks, UPDATE THE STATE WITH HOOKS

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Dallas, TX";
+  // The first part is always the current state the second part is the update
+
+  const [location, setLocation] = useState("Dallas, TX");
 
   return (
     <div className="search-params">
       <h1>Search-params</h1>
 
+      <p>{location}</p>
+
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
