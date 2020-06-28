d6ef17bab1122666d890253c2bdbbcfdbe6f5b24
Bruno DaSilva <hi.dasilvab@gmail.com>
2020-06-28 15:27:41 -0500

    NAIMALS API CALLED WITH NEW FORM

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   // The first part is always the current state the second part is the update
 
   const [location, setLocation] = useState("Dallas, TX");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -20,6 +22,23 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            //Fixing accessability Issue
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((data, i) => (
+              <option key={i} value={data}>
+                {data}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
